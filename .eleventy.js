afbd9a930676e321ea0ff94cd153e329b026facd
hrothmar <hrothmar.tv@gmail.com>
2023-10-31 21:35:53 -0700

    hooking up Sass to build successfully, then to get pulled into 11ty URLs successfully.

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,16 @@ const moment = require('moment');
 moment.locale('en');
  
 module.exports = function (eleventyConfig) {
- 
-  eleventyConfig.addFilter('dateIso', date => {
-    return moment(date).toISOString();
-  });
- 
-  eleventyConfig.addFilter('dateReadable', date => {
-    return moment(date).utc().format('LL'); // E.g. May 31, 2019
-  });
+
+	// Adds Sass directory as a trigger to recompile the site upon save
+	eleventyConfig.addWatchTarget("./src/sass");
+	eleventyConfig.addPassthroughCopy("bundle.css");
+
+	eleventyConfig.addFilter('dateIso', date => {
+		return moment(date).toISOString();
+	});
+
+	eleventyConfig.addFilter('dateReadable', date => {
+		return moment(date).utc().format('LL'); // E.g. May 31, 2019
+	});
 };
