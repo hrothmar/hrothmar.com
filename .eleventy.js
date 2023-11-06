c677fa0cdc37b70f80e3782fe368fe3f846948da
hrothmar <hrothmar.tv@gmail.com>
2023-11-06 09:41:28 -0800

    Adding Fonts and Images, then linking them up to the site

--- .eleventy.js
+++ .eleventy.js
@@ -4,14 +4,17 @@ moment.locale('en');
  
 module.exports = function (eleventyConfig) {
 
+	// Copy `assets/` to `_site/assets`
+	eleventyConfig.addPassthroughCopy("assets/"); 	
+
 	// Adds Sass directory as a trigger to recompile the site upon save
 	eleventyConfig.addWatchTarget("./src/sass");
 	eleventyConfig.addPassthroughCopy("bundle.css");
 
+	// Better Dates
 	eleventyConfig.addFilter('dateIso', date => {
 		return moment(date).toISOString();
 	});
-
 	eleventyConfig.addFilter('dateReadable', date => {
 		return moment(date).utc().format('LL'); // E.g. May 31, 2019
 	});
